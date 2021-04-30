d6bbeafe3843b609aeca769a99ad9c6f4fbe2076
Carlos Duque <carlos.duquerivera.0308@gmail.com>
2021-04-30 14:07:56 -0400

    initial commit

---
+++ public/js/firebaseSDKsnippet.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBRxkcFh3Ci9Lq58VtQnaFIrNKgi8pqSoc",
+    authDomain: "bcs130finalproject-3dd02.firebaseapp.com",
+    projectId: "bcs130finalproject-3dd02",
+    storageBucket: "bcs130finalproject-3dd02.appspot.com",
+    messagingSenderId: "236900805041",
+    appId: "1:236900805041:web:a22220f51b14bcf2adf590"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
