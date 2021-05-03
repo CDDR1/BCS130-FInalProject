3f8f4f9d8e0acfc7ac3ac3841c1dcb370edbc96a
Carlos Duque <carlos.duquerivera.0308@gmail.com>
2021-05-02 21:44:29 -0400

    adding image gallery

---
+++ public/js/about.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button")
+
+button.addEventListener("click", () => {
+    alert("Thanks for your message, we'll get in touch with you as soon as possible")
+})
+
